d50fc528bb2281dd419d430bdbff0f3e5e28fb5b
Nicholas <nicholasjalderton@gmail.com>
2023-12-08 17:33:26 +0000

    Ripped out functions, as there's no need for them

--- AoC2023/Day08/Day08.cpp
+++ AoC2023/Day08/Day08.cpp
@@ -15,48 +15,21 @@ namespace AoC2023 {
             return paths;
         }
 
-        size_t GetDistance(const std::string& inst, const std::unordered_map<int, std::pair<int, int>>& paths, int start, int end) {
-            size_t distance = 0;
-            while (start != end) {
-                if (inst[distance++ % inst.length()] == 'L') {
-                    start = paths.at(start).first;
-                }
-                else {
-                    start = paths.at(start).second;
-                }
-                if (distance > 1e6) {
-                    return std::numeric_limits<size_t>::max();
-                }
-            }
-            return distance;
-        }
-
-        std::unordered_map<int, int> GetDistances(const std::string& inst, const std::unordered_map<int, std::pair<int, int>>& paths) {
-            std::unordered_map<int, int> output = {};
-
-            for (auto& [start, _] : paths) {
-                if (start % 26 != 0) { continue; }
-                size_t pos = start;
-                size_t distance = 0;
-                while (pos % 26 != 25) {
-                    if (inst[distance++ % inst.length()] == 'L') {
-                        pos = paths.at(pos).first;
-                    }
-                    else {
-                        pos = paths.at(pos).second;
-                    }
-                }
-                output[start] = distance;
-            }
-
-            return output;
-        }
-
         std::pair<uint64_t, std::chrono::duration<double, std::milli>> A(const std::vector<std::string>& input) {
             auto starttime = std::chrono::high_resolution_clock::now();
 
             auto paths = LoadPaths(input);
-            uint64_t score = GetDistance(input[0], paths, 0, 17575);
+            size_t score = 0;
+
+            size_t pos = 0;         // AAA = 0
+            while (pos != 17575) {  // ZZZ = 17575
+                if (input[0][score++ % input[0].length()] == 'L') {
+                    pos = paths.at(pos).first;
+                }
+                else {
+                    pos = paths.at(pos).second;
+                }
+            }
 
             auto endtime = std::chrono::high_resolution_clock::now();
             return { score, endtime - starttime };
@@ -66,9 +39,20 @@ namespace AoC2023 {
             auto startTime = std::chrono::high_resolution_clock::now();
 
             auto paths = LoadPaths(input);
-            auto startEndDistances = GetDistances(input[0], paths);
             uint64_t score = 1;
-            for (auto& [_, distance] : startEndDistances) {
+
+            for (auto& [start, _] : paths) {
+                if (start % 26 != 0) { continue; }
+                size_t pos = start;
+                size_t distance = 0;
+                while (pos % 26 != 25) {
+                    if (input[0][distance++ % input[0].length()] == 'L') {
+                        pos = paths.at(pos).first;
+                    }
+                    else {
+                        pos = paths.at(pos).second;
+                    }
+                }
                 score = std::_Lcm(score, distance);
             }
 
@@ -77,16 +61,3 @@ namespace AoC2023 {
         }
     }
 }
-
-/*
-LRZ
-
-AAA = (AAB, XXX)
-AAB = (XXX, AAZ)
-AAZ = (AAB, XXX)
-ZZA = (ZZB, XXX)
-ZZB = (ZZC, ZZC)
-ZZC = (ZZZ, ZZZ)
-ZZZ = (ZZB, ZZB)
-XXX = (XXX, XXX)
-*/
--- AoC2023/Day08/Day08.h
+++ AoC2023/Day08/Day08.h
@@ -10,8 +10,6 @@
 namespace AoC2023 {
     namespace Day08 {
         std::unordered_map<int, std::pair<int, int>> LoadPaths(const std::vector<std::string>& input);
-        size_t GetDistance(const std::string& inst, const std::unordered_map<int, std::pair<int, int>>& paths, int start, int end);
-        std::unordered_map<int, int> GetDistances(const std::string& inst, const std::unordered_map<int, std::pair<int, int>>& paths);
 
         std::pair<uint64_t, std::chrono::duration<double, std::milli>> A(const std::vector<std::string>& input);
         std::pair<uint64_t, std::chrono::duration<double, std::milli>> B(const std::vector<std::string>& input);
