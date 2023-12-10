d10b3ab1b753c3d3f2c7af934a31da60458eab88
Nicholas <nicholasjalderton@gmail.com>
2023-12-10 17:18:37 +0000

    Day 10 - Total disaster, fine with method, but implementation needs a redo

--- AoC2023/Day10/Day10.cpp
+++ AoC2023/Day10/Day10.cpp
@@ -2,70 +2,258 @@
 
 #include <iostream>
 #include <numeric>
+#include <queue>
 
 namespace AoC2023::Day10 {
-    std::vector<int64_t> ParseLine(const std::string& input) {
-        std::vector<int64_t> data = {};
-
-        std::istringstream iss(input);
-        int64_t token;
-        while (iss >> token) {
-            data.push_back(token);
+    std::vector<Pipe> ParseLine(const std::string& input) {
+        std::vector<Pipe> output = {};
+        for (auto& c : input) {
+            switch (c) {
+            case '|':
+                output.push_back(Pipe::UP_DOWN);
+                break;
+            case '-':
+                output.push_back(Pipe::LEFT_RIGHT);
+                break;
+            case 'L':
+                output.push_back(Pipe::UP_RIGHT);
+                break;
+            case 'J':
+                output.push_back(Pipe::UP_LEFT);
+                break;
+            case '7':
+                output.push_back(Pipe::DOWN_LEFT);
+                break;
+            case 'F':
+                output.push_back(Pipe::DOWN_RIGHT);
+                break;
+            case '.':
+                output.push_back(Pipe::GROUND);
+                break;
+            case 'S':
+                output.push_back(Pipe::START);
+                break;
+            }
         }
-
-        return data;
+        return output;
     }
 
-    int64_t Process(const std::vector<int64_t>& data, const ExtrapolationFunction& extrapFunc) {
-        bool all0s = true;
-        std::vector<int64_t> newData = {};
-        for (size_t i = 0; i < data.size() - 1; i++) {
-            newData.push_back(data[i + 1] - data[i]);
-            if (newData[newData.size() - 1] != 0) {
-                all0s = false;
+    std::vector<std::vector<Pipe>> ParseGrid(const std::vector<std::string>& input) {
+        std::vector<std::vector<Pipe>> output = {};
+        for (auto& line : input) {
+            output.push_back(ParseLine(line));
+        }
+        return output;
+    }
+
+    size_t InflateAndFillGrid(const std::vector<std::vector<Pipe>>& grid) {
+        std::vector<std::vector<int>> output = {};
+        output.push_back({0,0,0,0,0,0});
+        output.push_back({ 0,0,0,0,0,0 });
+        output.push_back({ 0,0,0,0,0,0 });
+        for (size_t x = 0; x < grid[0].size(); x++) {
+            output[output.size() - 1].push_back(0); output[output.size() - 1].push_back(0); output[output.size() - 1].push_back(0);
+            output[output.size() - 2].push_back(0); output[output.size() - 2].push_back(0); output[output.size() - 2].push_back(0);
+            output[output.size() - 3].push_back(0); output[output.size() - 3].push_back(0); output[output.size() - 3].push_back(0);
+        }
+        for (size_t y = 0; y < grid.size(); y++) {
+            output.push_back({});
+            output.push_back({});
+            output.push_back({});
+            output[output.size() - 1].push_back(0); output[output.size() - 1].push_back(0); output[output.size() - 1].push_back(0);
+            output[output.size() - 2].push_back(0); output[output.size() - 2].push_back(0); output[output.size() - 2].push_back(0);
+            output[output.size() - 3].push_back(0); output[output.size() - 3].push_back(0); output[output.size() - 3].push_back(0);
+            for (size_t x = 0; x < grid[y].size(); x++) {
+                switch (grid[y][x]) {
+                case Pipe::UP_DOWN:
+                    output[output.size() - 3].push_back(0); output[output.size() - 3].push_back(1); output[output.size() - 3].push_back(0);
+                    output[output.size() - 2].push_back(0); output[output.size() - 2].push_back(1); output[output.size() - 2].push_back(0);
+                    output[output.size() - 1].push_back(0); output[output.size() - 1].push_back(1); output[output.size() - 1].push_back(0);
+                    break;
+                case Pipe::LEFT_RIGHT:
+                    output[output.size() - 3].push_back(0); output[output.size() - 3].push_back(0); output[output.size() - 3].push_back(0);
+                    output[output.size() - 2].push_back(1); output[output.size() - 2].push_back(1); output[output.size() - 2].push_back(1);
+                    output[output.size() - 1].push_back(0); output[output.size() - 1].push_back(0); output[output.size() - 1].push_back(0);
+                    break;
+                case Pipe::UP_RIGHT:
+                    output[output.size() - 3].push_back(0); output[output.size() - 3].push_back(1); output[output.size() - 3].push_back(0);
+                    output[output.size() - 2].push_back(0); output[output.size() - 2].push_back(1); output[output.size() - 2].push_back(1);
+                    output[output.size() - 1].push_back(0); output[output.size() - 1].push_back(0); output[output.size() - 1].push_back(0);
+                    break;
+                case Pipe::UP_LEFT:
+                    output[output.size() - 3].push_back(0); output[output.size() - 3].push_back(1); output[output.size() - 3].push_back(0);
+                    output[output.size() - 2].push_back(1); output[output.size() - 2].push_back(1); output[output.size() - 2].push_back(0);
+                    output[output.size() - 1].push_back(0); output[output.size() - 1].push_back(0); output[output.size() - 1].push_back(0);
+                    break;
+                case Pipe::DOWN_LEFT:
+                    output[output.size() - 3].push_back(0); output[output.size() - 3].push_back(0); output[output.size() - 3].push_back(0);
+                    output[output.size() - 2].push_back(1); output[output.size() - 2].push_back(1); output[output.size() - 2].push_back(0);
+                    output[output.size() - 1].push_back(0); output[output.size() - 1].push_back(1); output[output.size() - 1].push_back(0);
+                    break;
+                case Pipe::DOWN_RIGHT:
+                    output[output.size() - 3].push_back(0); output[output.size() - 3].push_back(0); output[output.size() - 3].push_back(0);
+                    output[output.size() - 2].push_back(0); output[output.size() - 2].push_back(1); output[output.size() - 2].push_back(1);
+                    output[output.size() - 1].push_back(0); output[output.size() - 1].push_back(1); output[output.size() - 1].push_back(0);
+                    break;
+                case Pipe::GROUND:
+                    output[output.size() - 3].push_back(0); output[output.size() - 3].push_back(0); output[output.size() - 3].push_back(0);
+                    output[output.size() - 2].push_back(0); output[output.size() - 2].push_back(0); output[output.size() - 2].push_back(0);
+                    output[output.size() - 1].push_back(0); output[output.size() - 1].push_back(0); output[output.size() - 1].push_back(0);
+                    break;
+                case Pipe::START:
+                    output[output.size() - 3].push_back(1); output[output.size() - 3].push_back(1); output[output.size() - 3].push_back(1);
+                    output[output.size() - 2].push_back(1); output[output.size() - 2].push_back(1); output[output.size() - 2].push_back(1);
+                    output[output.size() - 1].push_back(1); output[output.size() - 1].push_back(1); output[output.size() - 1].push_back(1);
+                    break;
+                }
+            }
+            output[output.size() - 1].push_back(0); output[output.size() - 1].push_back(0); output[output.size() - 1].push_back(0);
+            output[output.size() - 2].push_back(0); output[output.size() - 2].push_back(0); output[output.size() - 2].push_back(0);
+            output[output.size() - 3].push_back(0); output[output.size() - 3].push_back(0); output[output.size() - 3].push_back(0);
+        }
+        output.push_back({ 0,0,0,0,0,0 });
+        output.push_back({ 0,0,0,0,0,0 });
+        output.push_back({ 0,0,0,0,0,0 });
+        for (size_t x = 0; x < grid[0].size(); x++) {
+            output[output.size() - 1].push_back(0); output[output.size() - 1].push_back(0); output[output.size() - 1].push_back(0);
+            output[output.size() - 2].push_back(0); output[output.size() - 2].push_back(0); output[output.size() - 2].push_back(0);
+            output[output.size() - 3].push_back(0); output[output.size() - 3].push_back(0); output[output.size() - 3].push_back(0);
+        }
+
+        std::set<std::pair<size_t, size_t>> visited = {};
+        std::queue<std::pair<size_t, size_t>> queue = {};
+
+        std::pair<size_t, size_t> pos = { 1, 1 };
+        visited.insert(pos);
+        queue.push(pos);
+
+        while (queue.size()) {
+            auto currentPos = queue.front(); queue.pop();
+
+            for (auto& neighbour : GetAllNeighbours(output, currentPos)) {
+                if (output[neighbour.second][neighbour.first] == 0 && !visited.contains(neighbour)) {
+                    output[neighbour.second][neighbour.first] = 1;
+                    visited.insert(neighbour);
+                    queue.push(neighbour);
+                }
             }
         }
 
-        return extrapFunc(data, newData, all0s);
+        size_t result = 0;
+        for (size_t y = 4; y < output.size() - 3; y += 3) {
+            for (size_t x = 4; x < output[y].size() - 3; x += 3) {
+                size_t c = 0;
+                for (int dx = -1; dx < 2; dx++ ) {
+                    for (int dy = -1; dy < 2; dy++) {
+                        c += output[(int)y + dy][(int)x + dx];
+                    }
+                }
+                if (c == 3 || c == 0) {
+                    result++;
+                }
+            }
+        }
+        return result;
     }
 
-    int64_t ExtrapolateA(const std::vector<int64_t>& data, const std::vector<int64_t>& newData, bool all0s) {
-        if (all0s) {
-            return data[data.size() - 1];
+    std::pair<size_t, size_t> FindStart(const std::vector<std::vector<Pipe>>& grid) {
+        for (size_t y = 0; y < grid.size(); y++) {
+            for (size_t x = 0; x < grid[y].size(); x++) {
+                if (grid[y][x] == Pipe::START) {
+                    return { x, y };
+                }
+            }
         }
+        return { 0, 0 };
+    }
+
+    std::vector<std::pair<size_t, size_t>> GetPathNeighbours(const std::vector<std::vector<Pipe>>& grid, const std::pair<size_t, size_t>& pos) {
+        std::vector<std::pair<size_t, size_t>> neighbours = {};
+        auto pipe = grid[pos.second][pos.first];
+
+        if (pipe == Pipe::START) {
+            auto up = pos.second > 0 ? grid[pos.second - 1][pos.first] : Pipe::GROUND;
+            auto down = pos.second < grid.size() - 1 ? grid[pos.second + 1][pos.first] : Pipe::GROUND;
+            auto left = pos.first > 0 ? grid[pos.second][pos.first - 1] : Pipe::GROUND;
+            auto right = pos.first < grid[pos.second].size() - 1 ? grid[pos.second][pos.first + 1] : Pipe::GROUND;
+
+            if ((up == Pipe::UP_DOWN || up == Pipe::DOWN_LEFT || up == Pipe::DOWN_RIGHT) && pos.second < grid.size() - 1) { neighbours.push_back({ pos.first, pos.second - 1 }); }
+            if ((down == Pipe::UP_DOWN || down == Pipe::UP_RIGHT || down == Pipe::UP_LEFT) && pos.second > 0) { neighbours.push_back({ pos.first, pos.second + 1 }); }
+            if ((left == Pipe::LEFT_RIGHT || left == Pipe::UP_RIGHT || left == Pipe::DOWN_RIGHT) && pos.first < grid[pos.second].size() - 1) { neighbours.push_back({ pos.first - 1, pos.second }); }
+            if ((right == Pipe::LEFT_RIGHT || right == Pipe::UP_LEFT || right == Pipe::DOWN_LEFT) && pos.first > 0) { neighbours.push_back({ pos.first + 1, pos.second }); }
+        }
+
         else {
-            return data[data.size() - 1] + Process(newData, ExtrapolateA);
+            if ((pipe == Pipe::UP_DOWN || pipe == Pipe::UP_RIGHT || pipe == Pipe::UP_LEFT) && pos.second > 0) { neighbours.push_back({ pos.first, pos.second - 1 }); }
+            if ((pipe == Pipe::UP_DOWN || pipe == Pipe::DOWN_LEFT || pipe == Pipe::DOWN_RIGHT) && pos.second < grid.size() - 1) { neighbours.push_back({ pos.first, pos.second + 1 }); }
+            if ((pipe == Pipe::LEFT_RIGHT || pipe == Pipe::UP_LEFT || pipe == Pipe::DOWN_LEFT) && pos.first > 0) { neighbours.push_back({ pos.first - 1, pos.second }); }
+            if ((pipe == Pipe::LEFT_RIGHT || pipe == Pipe::UP_RIGHT || pipe == Pipe::DOWN_RIGHT) && pos.first < grid[pos.second].size() - 1) { neighbours.push_back({ pos.first + 1, pos.second }); }
         }
+
+        return neighbours;
     }
 
-    int64_t ExtrapolateB(const std::vector<int64_t>& data, const std::vector<int64_t>& newData, bool all0s) {
-        if (all0s) {
-            return data[0];
+    std::vector<std::pair<size_t, size_t>> GetAllNeighbours(const std::vector<std::vector<int>>& grid, const std::pair<size_t, size_t>& pos) {
+        std::vector<std::pair<size_t, size_t>> neighbours = {};
+
+        for (int dy = -1; dy < 2; dy++) {
+            if (dy == -1 && pos.second <= 0) { continue; }
+            if (dy == 1 && pos.second >= grid.size() - 1) { continue; }
+
+            for (int dx = -1; dx < 2; dx++) {
+                if (dx == 0 && dy == 0) { continue; }
+                if (dx == -1 && pos.first <= 0) { continue; }
+                if (dx == 1 && pos.first >= grid[pos.second + dy].size() - 1) { continue; }
+
+                neighbours.push_back({ pos.first + dx, pos.second + dy });
+            }
         }
-        else {
-            return data[0] - Process(newData, ExtrapolateB);
-        }
+
+        return neighbours;
     }
 
-    std::pair<uint64_t, std::chrono::duration<double, std::milli>> Solve(const std::vector<std::string>& input, const ExtrapolationFunction& extrapFunc) {
+    std::set<std::pair<size_t, size_t>> FindPath(const std::vector<std::vector<Pipe>>& grid, const std::pair<size_t, size_t>& pos) {
+        std::set<std::pair<size_t, size_t>> visited = {};
+        std::queue<std::pair<size_t, size_t>> queue = {};
+
+        visited.insert(pos);
+        queue.push(pos);
+
+        uint64_t size = 0;
+        while (queue.size()) {
+            auto currentPos = queue.front(); queue.pop();
+            size++;
+
+            for (auto& neighbour : GetPathNeighbours(grid, currentPos)) {
+                if (!visited.contains(neighbour)) {
+                    visited.insert(neighbour);
+                    queue.push(neighbour);
+                }
+            }
+        }
+
+        return visited;
+    }
+
+    std::pair<uint64_t, std::chrono::duration<double, std::milli>> A(const std::vector<std::string>& input) {
         auto starttime = std::chrono::high_resolution_clock::now();
 
-        int64_t score = 0;
-        for (auto& line : input) {
-            auto data = ParseLine(line);
-
-            score += Process(data, extrapFunc);
-        }
+        auto grid = ParseGrid(input);
+        auto start = FindStart(grid);
+        auto path = FindPath(grid, start);
+        auto score = path.size() / 2;   // Furthest difference through a loop is half its size
 
         auto endtime = std::chrono::high_resolution_clock::now();
         return { score, endtime - starttime };
     }
 
-    std::pair<uint64_t, std::chrono::duration<double, std::milli>> A(const std::vector<std::string>& input) {
-        return Solve(input, ExtrapolateA);
-    }
-
     std::pair<uint64_t, std::chrono::duration<double, std::milli>> B(const std::vector<std::string>& input) {
-        return Solve(input, ExtrapolateB);
+        auto starttime = std::chrono::high_resolution_clock::now();
+
+        auto grid = ParseGrid(input);
+        uint64_t score = InflateAndFillGrid(grid);
+
+        auto endtime = std::chrono::high_resolution_clock::now();
+        return { score, endtime - starttime };
     }
 }
--- AoC2023/Day10/Day10.h
+++ AoC2023/Day10/Day10.h
@@ -7,16 +7,31 @@
 #include <string>
 #include <unordered_map>
 #include <functional>
+#include <set>
 
 namespace AoC2023::Day10 {
-    typedef std::function<int64_t(const std::vector<int64_t>& data, const std::vector<int64_t>& newData, bool all0s)> ExtrapolationFunction;
+    enum class Pipe {
+        UP_DOWN,
+        LEFT_RIGHT,
+        UP_RIGHT,
+        UP_LEFT,
+        DOWN_LEFT,
+        DOWN_RIGHT,
+        GROUND,
+        START
+    };
 
-    std::vector<int64_t> ParseLine(const std::string& input);
-    int64_t Process(const std::vector<int64_t>& data, const ExtrapolationFunction& extrapFunc);
-    int64_t ExtrapolateA(const std::vector<int64_t>& data, const std::vector<int64_t>& newData, bool all0s);
-    int64_t ExtrapolateB(const std::vector<int64_t>& data, const std::vector<int64_t>& newData, bool all0s);
+    std::vector<Pipe> ParseLine(const std::string& input);
+    std::vector<std::vector<Pipe>> ParseGrid(const std::vector<std::string>& input);
 
-    std::pair<uint64_t, std::chrono::duration<double, std::milli>> Solve(const std::vector<std::string>& input, const ExtrapolationFunction& extrapFunc);
+    std::pair<size_t, size_t> FindStart(const std::vector<std::vector<Pipe>>& grid);
+
+    std::vector<std::pair<size_t, size_t>> GetPathNeighbours(const std::vector<std::vector<Pipe>>& grid, const std::pair<size_t, size_t>& pos);
+    std::vector<std::pair<size_t, size_t>> GetAllNeighbours(const std::vector<std::vector<int>>& grid, const std::pair<size_t, size_t>& pos);
+
+    std::set<std::pair<size_t, size_t>> FindPath(const std::vector<std::vector<Pipe>>& grid, const std::pair<size_t, size_t>& pos);
+    uint64_t FindVolume(const std::vector<std::vector<Pipe>>& grid, const std::set<std::pair<size_t, size_t>>& path);
+
     std::pair<uint64_t, std::chrono::duration<double, std::milli>> A(const std::vector<std::string>& input);
     std::pair<uint64_t, std::chrono::duration<double, std::milli>> B(const std::vector<std::string>& input);
 }
