630ba90cc0332b6feb12b1425e6ed065e489ce54
Nicholas <nicholasjalderton@gmail.com>
2023-12-14 01:10:45 +0000

    Day 13 function declarations.

--- AoC2023/Day13/Day13.h
+++ AoC2023/Day13/Day13.h
@@ -9,6 +9,14 @@
 namespace AoC2023::Day13 {
     std::vector<std::vector<std::vector<bool>>> ParseInput(const std::vector<std::string>& input);
 
+    bool VerifyHorizontalReflection(const std::vector<std::vector<bool>>& grid, int checkCol, int row);
+    int GetHorizontalReflection(const std::vector<std::vector<bool>>& grid);
+    int GetHorizontalReflectionB(std::vector<std::vector<bool>>& grid);
+
+    bool VerifyVerticalReflection(const std::vector<std::vector<bool>>& grid, int checkRow, int col);
+    int GetVerticalReflection(const std::vector<std::vector<bool>>& grid);
+    int GetVerticalReflectionB(std::vector<std::vector<bool>>& grid);
+
     std::pair<uint64_t, std::chrono::duration<double, std::milli>> A(const std::vector<std::string>& input);
     std::pair<uint64_t, std::chrono::duration<double, std::milli>> B(const std::vector<std::string>& input);
 }
