0c8307bf70d8d5ceef92b84579ed837c0cedca49
Nicholas <nicholasjalderton@gmail.com>
2023-12-16 21:56:58 +0000

    Function declaration

--- AoC2023/Day16/Day16.h
+++ AoC2023/Day16/Day16.h
@@ -7,6 +7,8 @@
 #include <string>
 
 namespace AoC2023::Day16 {
+    size_t Run(const std::vector<std::string>& grid, std::pair<std::pair<int, int>, std::pair<int, int>> startBeam);
+
     std::tuple<uint64_t, std::chrono::duration<double, std::milli>, std::chrono::duration<double, std::milli>> A(const std::vector<std::string>& input);
     std::tuple<uint64_t, std::chrono::duration<double, std::milli>, std::chrono::duration<double, std::milli>> B(const std::vector<std::string>& input);
 }
