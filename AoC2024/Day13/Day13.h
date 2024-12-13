e432a8f226cb35d30ab06bf64b3a393db78bc7ca
Nicholas <nicholasjalderton@gmail.com>
2024-12-13 07:51:39 +0000

    update header

--- AoC2024/Day13/Day13.h
+++ AoC2024/Day13/Day13.h
@@ -9,6 +9,7 @@
 namespace AoC2024 {
     namespace Day13 {
         std::vector<std::tuple<std::pair<uint64_t, uint64_t>, std::pair<uint64_t, uint64_t>, std::pair<uint64_t, uint64_t>>> PreProcessInput(const std::vector<std::string>& input);
+        uint64_t Solve(const std::tuple<std::pair<uint64_t, uint64_t>, std::pair<uint64_t, uint64_t>, std::pair<uint64_t, uint64_t>>& testCase);
         std::pair<uint64_t, std::chrono::duration<double, std::milli>> A(const std::vector<std::string>& input);
         std::pair<uint64_t, std::chrono::duration<double, std::milli>> B(const std::vector<std::string>& input);
     }
