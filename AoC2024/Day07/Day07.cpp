0dbff146cb13be6dbe047e3e1aaeff45145b6c1e
Nicholas <nicholasjalderton@gmail.com>
2024-12-20 14:44:25 +0000

    Day 7

--- AoC2024/Day07/Day07.cpp
+++ AoC2024/Day07/Day07.cpp
@@ -1,63 +1,75 @@
+#include <sstream>
+#include <numeric>
+
 #include "Day07.h"
 
+#include "../../Helpers/Helpers.h"
+
 namespace AoC2024 {
-    namespace Day07 {
-        bool Valid(const std::vector<uint64_t>& eq, bool withConcat) {
-            return Valid(eq[0], eq[1], eq, 2, withConcat);
-        }
+    void Day07::Load() {
+        input = AoC::Helpers::ReadLines("./Day07.txt");
+    }
 
-        bool Valid(uint64_t solution, uint64_t current, const std::vector<uint64_t>& eq, int i, bool withConcat) {
-            if (eq.size() == i) {
-                return solution == current;
+    void Day07::Parse() {
+        for (auto& line : input) {
+            std::vector<uint64_t> eq = {};
+            auto s = std::stringstream(line);
+            std::string tk;
+            while (s >> tk) {
+                eq.push_back(std::stoull(tk));
             }
-            else {
-                if(current > solution){ return false; }
-                return Valid(solution, current + eq[i], eq, i + 1, withConcat)
-                    || Valid(solution, current * eq[i], eq, i + 1, withConcat)
-                    || (withConcat && Valid(solution, Concat(current, eq[i]), eq, i + 1, withConcat));
+            equations.push_back(eq);
+        }
+    }
+
+    void Day07::A() {
+        auto startTime = std::chrono::high_resolution_clock::now();
+
+        uint64_t res = Run();
+
+        auto endTime = std::chrono::high_resolution_clock::now();
+        partAResult = { res, endTime - startTime };
+    }
+
+    void Day07::B() {
+        auto startTime = std::chrono::high_resolution_clock::now();
+
+        uint64_t res = Run(true);
+
+        auto endTime = std::chrono::high_resolution_clock::now();
+        partBResult = { res, endTime - startTime };
+    }
+
+    bool Day07::Valid(const std::vector<uint64_t>& eq, bool withConcat) {
+        return Valid(eq[0], eq[1], eq, 2, withConcat);
+    }
+
+    bool Day07::Valid(uint64_t solution, uint64_t current, const std::vector<uint64_t>& eq, int i, bool withConcat) {
+        if (eq.size() == i) {
+            return solution == current;
+        }
+        else {
+            if (current > solution) { return false; }
+            return Valid(solution, current + eq[i], eq, i + 1, withConcat)
+                || Valid(solution, current * eq[i], eq, i + 1, withConcat)
+                || (withConcat && Valid(solution, Concat(current, eq[i]), eq, i + 1, withConcat));
+        }
+    }
+
+    uint64_t Day07::Concat(uint64_t x, uint64_t y) {
+        uint64_t pow = 10;
+        while (y >= pow) { pow *= 10; }
+        return x * pow + y;
+    }
+
+    uint64_t Day07::Run(bool withConcat) {
+        return std::accumulate(
+            equations.begin(),
+            equations.end(),
+            0ui64,
+            [&](const uint64_t acc, const std::vector<uint64_t> eq) {
+                return acc + (Valid(eq, withConcat) ? eq[0] : 0);
             }
-        }
-
-        uint64_t Concat(uint64_t x, uint64_t y) {
-            uint64_t pow = 10;
-            while (y >= pow){ pow *= 10; }
-            return x * pow + y;
-        }
-
-        uint64_t Run(const std::vector<std::string>& input, bool withConcat) {
-            uint64_t res = 0;
-
-            for (auto& line : input) {
-                std::vector<uint64_t> eq = {};
-                auto s = std::stringstream(line);
-                std::string tk;
-                while (s >> tk) {
-                    eq.push_back(std::stoull(tk));
-                }
-                if (Valid(eq, withConcat)) {
-                    res += eq[0];
-                }
-            }
-
-            return res;
-        }
-
-        std::pair<uint64_t, std::chrono::duration<double, std::milli>> A(const std::vector<std::string>& input) {
-            auto starttime = std::chrono::high_resolution_clock::now();
-
-            uint64_t res = Run(input);
-
-            auto endtime = std::chrono::high_resolution_clock::now();
-            return { res, endtime - starttime };
-        }
-
-        std::pair<uint64_t, std::chrono::duration<double, std::milli>> B(const std::vector<std::string>& input) {
-            auto startTime = std::chrono::high_resolution_clock::now();
-
-            uint64_t res = Run(input, true);
-
-            auto endtime = std::chrono::high_resolution_clock::now();
-            return { res, endtime - startTime };
-        }
+        );
     }
 }
--- AoC2024/Day07/Day07.h
+++ AoC2024/Day07/Day07.h
@@ -1,19 +1,34 @@
-#ifndef DAY07_H
-#define DAY07_H
-
-#include <cstdint>
+#pragma once
 #include <vector>
-#include <chrono>
 #include <string>
+#include <tuple>
+#include <cstdint>
+
+#include "../Day/Day.h"
 
 namespace AoC2024 {
-    namespace Day07 {
+    class Day07 : public AoC::Day {
+    public:
+        void Load() override;
+        void Parse() override;
+        void A() override;
+        void B() override;
+
+        Day07() : Day() {
+            dayNo = 7;
+            Load();
+            Parse();
+            A();
+            B();
+        }
+
+    private:
+        std::vector<std::string> input = {};
+        std::vector<std::vector<uint64_t>> equations = {};
+
         bool Valid(const std::vector<uint64_t>& eq, bool withConcat = false);
         bool Valid(uint64_t solution, uint64_t current, const std::vector<uint64_t>& eq, int i = 0, bool withConcat = false);
         uint64_t Concat(uint64_t x, uint64_t y);
-        uint64_t Run(const std::vector<std::string>& input, bool withConcat = false);
-        std::pair<uint64_t, std::chrono::duration<double, std::milli>> A(const std::vector<std::string>& input);
-        std::pair<uint64_t, std::chrono::duration<double, std::milli>> B(const std::vector<std::string>& input);
-    }
+        uint64_t Run(bool withConcat = false);
+    };
 }
-#endif
