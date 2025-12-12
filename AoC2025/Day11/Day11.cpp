7017f5a3cc3f01c5baec73d2c589bf803edf9461
Nicholas <9338660+Zeeveez@users.noreply.github.com>
2025-12-12 00:14:57 +0000

    string view split instead of manual parse

--- AoC2025/Day11/Day11.cpp
+++ AoC2025/Day11/Day11.cpp
@@ -1,51 +1,30 @@
 #include <stack>
+#include <ranges>
 
 #include "Day11.h"
 
 namespace AoC2025 {
-    void Day11::Parse() {
-        map = {};
-        ids = {};
-
-        for (auto& line : rawData) {
-            std::string src = "";
-            std::string id = "";
-            for (auto& c : line) {
-                if (std::isalpha(c)) { id += c; }
-                else if (id != "") {
-                    if (!ids.contains(id)) { ids[id] = ids.size(); }
-                    if (src == "") { src = id; }
-                    else {
-                        if (!map.contains(ids[src])) { map[ids[src]] = {}; }
-                        map[ids[src]].insert(ids[id]);
-                    }
-                    id = "";
-                }
-            }
-            if (id != "") {
-                if (!ids.contains(id)) { ids[id] = ids.size(); }
-                if (src == "") { src = id; }
-                else {
-                    if (!map.contains(ids[src])) { map[ids[src]] = {}; }
-                    map[ids[src]].insert(ids[id]);
-                }
-            }
+void Day11::Parse() {
+    map = {};
+    ids = {};
+    for (auto& line : rawData) {
+        int src = -1;
+        for (auto& rawId : std::ranges::to<std::vector<std::string>>(line | std::views::split(' '))) {
+            auto id = rawId.substr(0, 3); // trim :
+            if (!ids.contains(id)) { ids[id] = ids.size(); }
+            if (src == -1) { src = ids[id]; map[src] = {}; }
+            else { map[src].insert(ids[id]); }
         }
-
-        you = ids["you"];
-        out = ids["out"];
-        srv = ids["svr"];
-        dac = ids["dac"];
-        fft = ids["fft"];
     }
+}
 
     AoC::DayResult::PuzzleResult Day11::A() {
-        return CountRoutes(you, out);
+        return CountRoutes(ids["you"], ids["out"]);
     }
 
     AoC::DayResult::PuzzleResult Day11::B() {
-        return CountRoutes(srv, fft) * CountRoutes(fft, dac) * CountRoutes(dac, out)
-            + CountRoutes(srv, dac) * CountRoutes(dac, fft) * CountRoutes(fft, out);
+        return CountRoutes(ids["svr"], ids["fft"]) * CountRoutes(ids["fft"], ids["dac"]) * CountRoutes(ids["dac"], ids["out"])
+            + CountRoutes(ids["svr"], ids["dac"]) * CountRoutes(ids["dac"], ids["fft"]) * CountRoutes(ids["fft"], ids["out"]);
     }
 
     std::uint64_t Day11::CountRoutes(int from, int to) {
